edc9a68226bb9e1b982968e0063114f932b73467
vinicio Brejinski <viniciosdb@gmail.com>
2023-08-10 22:32:13 -0300

    find by id

---
+++ src/application/Program.java
@@ -0,0 +1,18 @@
+package application;
+
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
+
+public class Program {
+
+    public static void main(String[] args) {
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+        Seller seller = sellerDao.findById(4);
+        System.out.println(seller);
+
+    }
+}
